083a9c188a05edd49bb06565a0effe8f2d5abe6f
Нечто Страшное <naumov-1212@Mail.ru>
2022-12-05 22:36:33 +0300

    server

--- PipeC/PipeC.c
+++ PipeC/PipeC.c
@@ -3,28 +3,46 @@
 int main()
 {
 	system("chcp 1251");
+	HANDLE hNamePipe;
+	LPSTR pipeName = L"";
+	DWORD read_buffer = 100;
+	LPWSTR buffer = calloc(read_buffer, sizeof(char));
+	char message[100];
+	DWORD actual_read;
+	BOOL Connected;
+	BOOL SuccessRead;
 	while (1)
 	{
-		HANDLE hNamePipe;
-		LPSTR pipeName = L"";
-		hNamePipe = CreateNamedPipe(pipeName, PIPE_ACCESS_DUPLEX, PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT, PIPE_UNLIMITED_INSTANCES, 512, 512, INFINITE, NULL);
-
-		BOOL Connected = ConnectNamedPipe(hNamePipe, NULL);
+		hNamePipe = CreateNamedPipe(
+			pipeName, 
+			PIPE_ACCESS_DUPLEX, 
+			PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT, 
+			PIPE_UNLIMITED_INSTANCES, 
+			512, 
+			512, 
+			INFINITE, 
+			NULL);
+		Connected = ConnectNamedPipe(hNamePipe, NULL);
 		if (Connected) {
 			printf("\nКлиент подключился\n");
-			DWORD read_buffer = 100;
-			LPWSTR buffer = calloc(read_buffer, sizeof(char));
-
-			DWORD actual_read;
-			BOOL SuccessRead = ReadFile(hNamePipe, buffer, read_buffer, &actual_read, NULL);
-			if (!SuccessRead)
-				printf("\nКлиент отключился");
-
-			printf(buffer);
-			CloseHandle(hNamePipe);
+			SuccessRead = ReadFile(hNamePipe, buffer, read_buffer, &actual_read, NULL);
+			if (SuccessRead) {
+				printf("\nКлиент пишет");
+				printf(buffer);
+				printf("\n");
+				printf("\nВведите сообщение для клиента");
+				gets(message);
+				buffer = &message;
+				WriteFile(hNamePipe, buffer, read_buffer, &actual_read, NULL);
+			}
 		}
+		else
+		{
+			printf("\nКлиент отключился от сервера");
+		}
+			CloseHandle(hNamePipe);
 	}
-
 }
+
 
 
